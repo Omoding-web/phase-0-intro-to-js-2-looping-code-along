3d94d23aa16b1f04f784feef4146ebf30eecc634
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-18 12:41:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(startingNumber) {
+    let count = startingNumber;
+
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
+
+const thankYouMessages = (writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+console.log(thankYouMessages);
+
+countDown(10);
